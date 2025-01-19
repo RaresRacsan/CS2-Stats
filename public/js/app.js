05fe1e9688838cbd87f9825299673086e1ac7f7d
916-Racsan-Rares <82086767+RaresRacsan@users.noreply.github.com>
2025-01-19 20:10:17 +0200

    added a little bit of css to stats
    now the stats page has 3 sections

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const playerId = urlParams.get('id');
 
-if(!playerId) {
+if (!playerId) {
     window.location.href = 'index.html';
 }
 
@@ -26,63 +26,66 @@ function fetchPlayerStats() {
 
             // Calculate accuracy
             const accuracy = ((findStat('total_shots_hit') / findStat('total_shots_fired')) * 100).toFixed(1);
-            
+
             statsContainer.innerHTML = `
-                <div class="stats">
-                    <h2>Player Stats Overview</h2>
-                    <p>Total Matches: ${findStat('total_matches_played')}</p>
-                    <p>Matches Won: ${findStat('total_matches_won')}</p>
-                    <p>Win Rate: ${((findStat('total_matches_won') / findStat('total_matches_played')) * 100).toFixed(1)}%</p>
-                    <p>Total Kills: ${findStat('total_kills')}</p>
-                    <p>Total Deaths: ${findStat('total_deaths')}</p>
-                    <p>K/D Ratio: ${(findStat('total_kills') / findStat('total_deaths')).toFixed(2)}</p>
-                    <p>Headshots: ${findStat('total_kills_headshot')}</p>
-                    <p>Accuracy: ${accuracy}%</p>
-                    <p>MVPs: ${findStat('total_mvps')}</p>
-                    <p>Total damage done: ${findStat('total_damage_done')}</p>
-                    <p>Total money earned: $${findStat('total_money_earned')}</p>
-                    <p>Total planted bombs: ${findStat('total_planted_bombs')}</p>
-                    <p>Total defused bombs: ${findStat('total_defused_bombs')}</p>
-                    <p>Total resqued hostages: ${findStat('total_rescued_hostages')}</p>
-                    
-                    <h3>Top Weapons</h3>
-                    <p>AK-47: ${findStat('total_kills_ak47')} kills</p>
-                    <p>AWP: ${findStat('total_kills_awp')} kills</p>
-                    <p>M4A1: ${findStat('total_kills_m4a1')} kills</p>
-                    <p>Desert Eagle: ${findStat('total_kills_deagle')} kills</p>
-                    <p>Knife: ${findStat('total_kills_knife')} kills</p>
-                    <p>Grenade: ${findStat('total_kills_hegrenade')} kills</p>
-                    <p>Glock: ${findStat('total_kills_glock')} kills</p>
-                    <p>FiveSeven: ${findStat('total_kills_fiveseven')} kills</p>
-                    <p>USP: ${findStat('total_kills_hkp2000')} kills</p>
-                    <p>P2000: ${findStat('total_kills_hkp2000')} kills</p>
-                    <p>Xm1014: ${findStat('total_kills_xm1014')} kills</p>
-                    <p>Mac10: ${findStat('total_kills_mac10')} kills</p>
-                    <p>Ump45: ${findStat('total_kills_ump45')} kills</p>
-                    <p>P90: ${findStat('total_kills_p90')} kills</p>
-                    <p>Aug: ${findStat('total_kills_aug')} kills</p>
-                    <p>Famas: ${findStat('total_kills_famas')} kills</p>
-                    <p>Sg: ${findStat('total_kills_sg556')} kills</p>
-                    <p>M249: ${findStat('total_kills_m249')} kills</p>
-                    <p>P250: ${findStat('total_kills_p250')} kills</p>
-                    <p>Scar: ${findStat('total_kills_scar20')} kills</p>
-                    <p>SSG: ${findStat('total_kills_ssg08')} kills</p>
-                    <p>Mp7: ${findStat('total_kills_mp7')} kills</p>
-                    <p>Mp9: ${findStat('total_kills_mp9')} kills</p>
-                    <p>Nova: ${findStat('total_kills_nova')} kills</p>
-                    <p>Negev: ${findStat('total_kills_negev')} kills</p>
-                    <p>SawedOff: ${findStat('total_kills_sawedoff')} kills</p>
-                    <p>Bizon: ${findStat('total_kills_bizon')} kills</p>
-                    <p>Tec9: ${findStat('total_kills_tec9')} kills</p>
-                    <p>mag7: ${findStat('total_kills_mag7')} kills</p>
-                    <p>Galil: ${findStat('total_kills_galilar')} kills</p>
-                    <p>Taser: ${findStat('total_kills_taser')} kills</p>
-                    <p>Decoy: ${findStat('total_kills_decoy')} kills</p>
-                    <p>Molotov: ${findStat('total_kills_molotov')} kills</p>
-                    <p>M4a1S: ${findStat('total_kills_m4a1')} kills</p>
-                    <p>
-                    <p>Enemy Weapon: ${findStat('total_kills_enemy_weapon')} kills</p>
-                </div>
+                <div class="stats-container">
+                    <div class="general-stats">
+                        <h2>Player Stats Overview</h2>
+                        <p>Total Matches: ${findStat('total_matches_played')}</p>
+                        <p>Matches Won: ${findStat('total_matches_won')}</p>
+                        <p>Win Rate: ${((findStat('total_matches_won') / findStat('total_matches_played')) * 100).toFixed(1)}%</p>
+                        <p>Total Kills: ${findStat('total_kills')}</p>
+                        <p>Total Deaths: ${findStat('total_deaths')}</p>
+                        <p>K/D Ratio: ${(findStat('total_kills') / findStat('total_deaths')).toFixed(2)}</p>
+                        <p>Headshots: ${findStat('total_kills_headshot')}</p>
+                        <p>Accuracy: ${accuracy}%</p>
+                        <p>MVPs: ${findStat('total_mvps')}</p>
+                        <p>Total damage done: ${findStat('total_damage_done')}</p>
+                        <p>Total money earned: $${findStat('total_money_earned')}</p>
+                        <p>Total planted bombs: ${findStat('total_planted_bombs')}</p>
+                        <p>Total defused bombs: ${findStat('total_defused_bombs')}</p>
+                        <p>Enemy Weapon: ${findStat('total_kills_enemy_weapon')} kills</p>
+                        <p>Total resqued hostages: ${findStat('total_rescued_hostages')}</p>
+                    </div>
+
+                    <div class="weapon-stats">
+                        <h3>Top Weapons</h3>
+                        <p>AK-47: ${findStat('total_kills_ak47')} kills</p>
+                        <p>AWP: ${findStat('total_kills_awp')} kills</p>
+                        <p>M4A1: ${findStat('total_kills_m4a1')} kills</p>
+                        <p>Desert Eagle: ${findStat('total_kills_deagle')} kills</p>
+                        <p>Knife: ${findStat('total_kills_knife')} kills</p>
+                        <p>Grenade: ${findStat('total_kills_hegrenade')} kills</p>
+                        <p>Glock: ${findStat('total_kills_glock')} kills</p>
+                        <p>FiveSeven: ${findStat('total_kills_fiveseven')} kills</p>
+                        <p>USP: ${findStat('total_kills_hkp2000')} kills</p>
+                        <p>P2000: ${findStat('total_kills_hkp2000')} kills</p>
+                        <p>Xm1014: ${findStat('total_kills_xm1014')} kills</p>
+                        <p>Mac10: ${findStat('total_kills_mac10')} kills</p>
+                        <p>Ump45: ${findStat('total_kills_ump45')} kills</p>
+                        <p>P90: ${findStat('total_kills_p90')} kills</p>
+                        <p>Aug: ${findStat('total_kills_aug')} kills</p>
+                        <p>Famas: ${findStat('total_kills_famas')} kills</p>
+                        <p>Sg: ${findStat('total_kills_sg556')} kills</p>
+                        <p>M249: ${findStat('total_kills_m249')} kills</p>
+                        <p>P250: ${findStat('total_kills_p250')} kills</p>
+                        <p>Scar: ${findStat('total_kills_scar20')} kills</p>
+                        <p>SSG: ${findStat('total_kills_ssg08')} kills</p>
+                        <p>Mp7: ${findStat('total_kills_mp7')} kills</p>
+                        <p>Mp9: ${findStat('total_kills_mp9')} kills</p>
+                        <p>Nova: ${findStat('total_kills_nova')} kills</p>
+                        <p>Negev: ${findStat('total_kills_negev')} kills</p>
+                        <p>SawedOff: ${findStat('total_kills_sawedoff')} kills</p>
+                        <p>Bizon: ${findStat('total_kills_bizon')} kills</p>
+                        <p>Tec9: ${findStat('total_kills_tec9')} kills</p>
+                        <p>mag7: ${findStat('total_kills_mag7')} kills</p>
+                        <p>Galil: ${findStat('total_kills_galilar')} kills</p>
+                        <p>Taser: ${findStat('total_kills_taser')} kills</p>
+                        <p>Decoy: ${findStat('total_kills_decoy')} kills</p>
+                        <p>Molotov: ${findStat('total_kills_molotov')} kills</p>
+                        <p>M4a1S: ${findStat('total_kills_m4a1')} kills</p>
+                    </div>         
+                </div>           
             `;
         })
         .catch(error => {
@@ -146,7 +149,7 @@ function fetchMatchHistory() {
         });
 }
 
-function fetchPlayerInfo () {
+function fetchPlayerInfo() {
     const playerInfoContainer = document.getElementById('player-info');
     playerInfoContainer.innerHTML = '<p>Loading player info...</p>';
 
@@ -204,7 +207,7 @@ function fetchRecentGames() {
                     ` : '<p>No recent CS2 activity</p>'}
                 </div>
             `;
-            
+
             recentGamesContainer.innerHTML = recentGamesHTML;
         })
         .catch(error => {
